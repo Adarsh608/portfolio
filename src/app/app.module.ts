4817235abcbd1bec4b268e6d2bc77b67f25891eb
Adarsh rao <raoadarsh942@gmail.com.com>
2024-12-23 00:29:06 +0530

    module error fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ButtomTopComponent } from './buttom-top/buttom-top.component';
-import { NewComponentComponent } from './new-component/new-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ButtomTopComponent,
-    NewComponentComponent
   ],
   imports: [
     BrowserModule,
